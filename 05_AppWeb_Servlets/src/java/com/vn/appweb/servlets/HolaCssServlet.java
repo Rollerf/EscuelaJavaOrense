6894fcd01dce857ffb9b7e3bcd609bb6d95c1715
Chema <chema.sumay@gmail.com>
2019-11-20 18:09:12 +0100

    -Empezando con JSP

---
+++ 05_AppWeb_Servlets/src/java/com/vn/appweb/servlets/HolaCssServlet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vn.appweb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author PC
+ */
+public class HolaCssServlet extends HttpServlet{
+    
+    @Override
+    protected void doGet(HttpServletRequest peticionHttp,
+            HttpServletResponse respuestaHttp) throws IOException {
+        //definiamos el tiempo de contenido segun los tipos mime
+        // formatos conocidos de ficheros para emails
+        respuestaHttp.setContentType("text/css;");
+        try (PrintWriter salida = respuestaHttp.getWriter()) {
+            salida.print("body{");
+            salida.print("color:red;}");
+            
+        }
+    }
+    
+}
---
+++ 05_AppWeb_Servlets/src/java/com/vn/appweb/servlets/HolaHtmlServlet.java
@@ -0,0 +1,43 @@
+package com.vn.appweb.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet que genera una respuesta en formato HTML
+ *
+ * @author PC
+ */
+public class HolaHtmlServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest peticionHttp,
+            HttpServletResponse respuestaHttp) throws IOException {
+        //definiamos el tiempo de cconteniido segun los tipos mime
+        // formatos conocidosde ficheros para emails
+        respuestaHttp.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter salida = respuestaHttp.getWriter()) {
+            salida.print("<html>");
+            salida.print("<head>");
+            salida.print("<title>Web HTML desde Servlet</title>");
+            salida.print("<link rel='stylesheet' href='../saludos/holacss'>");
+            salida.print("</head>");
+            salida.print("<body>"
+                    + "<h1>Web HTML desde Servlet</h1>"
+                    + "<h2>Hola que pasa</h2>"
+                    + "<ul>");
+            for (int i = 0; i < 10; i++) {
+                salida.print("<li> Iteración " + i + "</li>");
+            }
+            salida.print("</ul>");
+            salida.println("<br/>Ruta: "+peticionHttp.getContextPath());
+            salida.println("<br/>Metodo: "+peticionHttp.getMethod());
+            salida.print("</body>");
+            salida.print("</html>");
+            //salida.close(); // ya no hace falta try se encarga de cerrar
+        }
+    }
+}
